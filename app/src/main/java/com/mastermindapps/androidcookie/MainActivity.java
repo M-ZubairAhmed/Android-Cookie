9ce5cb076686a387dffbc7083202beb5a6397f78
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-21 18:13:19 +0530

    button text added

--- app/src/main/java/com/mastermindapps/androidcookie/MainActivity.java
+++ app/src/main/java/com/mastermindapps/androidcookie/MainActivity.java
@@ -15,16 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ImageView cookieImage = (ImageView) findViewById(R.id.cookie_image);
-        Button eatButton = (Button) findViewById(R.id.eat_button);
+        final Button eatButton = (Button) findViewById(R.id.eat_button);
         eatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (cookieThere){
                     cookieImage.setImageResource(R.drawable.after_cookie);
+                    eatButton.setText(R.string.another_cookie);
                     cookieThere = false;
                 }
                 else {
                     cookieImage.setImageResource(R.drawable.before_cookie);
+                    eatButton.setText(R.string.eat_cookie);
                     cookieThere = true;
                 }
             }
