132f6137167294f39c69914ebd565a9bb2e3ec25
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-21 18:00:29 +0530

    button attached to listener

--- app/src/main/java/com/mastermindapps/androidcookie/MainActivity.java
+++ app/src/main/java/com/mastermindapps/androidcookie/MainActivity.java
@@ -2,14 +2,28 @@ package com.mastermindapps.androidcookie;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean cookieThere = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final ImageView cookieImage = (ImageView) findViewById(R.id.cookie_image);
+        Button eatButton = (Button) findViewById(R.id.eat_button);
+        eatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cookieThere){
+                    cookieImage.setImageResource(R.drawable.after_cookie);
+                    cookieThere = false;
+                }
+            }
+        });
     }
 }
